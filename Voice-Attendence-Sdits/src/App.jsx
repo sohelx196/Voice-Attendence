d4cdb62cc94aac4691119cecb06e25059f2451d3
sohelx196 <sohel.devloper196@gmail.com>
2025-03-06 10:56:25 +0530

    Chnged

--- Voice-Attendence-Sdits/src/App.jsx
+++ Voice-Attendence-Sdits/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-5xl text-center flex-col '>Helo World</h1>
+     <h1 className='text-5xl text-center flex-col '>Helo World..Rajat is here!!</h1>
     </>
   )
 }
