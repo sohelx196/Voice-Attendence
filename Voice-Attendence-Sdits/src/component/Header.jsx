b11f5629f0b5ec3e4534a223f07ede04bff77c96
sohelx196 <sohel.devloper196@gmail.com>
2025-03-09 09:49:42 +0530

    changedd

--- Voice-Attendence-Sdits/src/component/Header.jsx
+++ Voice-Attendence-Sdits/src/component/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavLink from "react-router-dom";
-import ".index.css";
+
 
 function Header() {
   return (
@@ -12,10 +12,10 @@ function Header() {
     <div className="bg-blue-300">
       <ul className="flex">
         <li className=""><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink></li>
-        <li className=""><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Attendence</NavLink></li>
-        <li className=""><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>New Student</NavLink></li>
-        <li className=""><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Register</NavLink></li>
-        <li className=""><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Admin</NavLink></li>
+        <li className=""><NavLink to="" className={({ isActive }) => (isActive ? "active" : "")}>Attendence</NavLink></li>
+        <li className=""><NavLink to="" className={({ isActive }) => (isActive ? "active" : "")}>New Student</NavLink></li>
+        <li className=""><NavLink to="" className={({ isActive }) => (isActive ? "active" : "")}>Register</NavLink></li>
+        <li className=""><NavLink to="" className={({ isActive }) => (isActive ? "active" : "")}>Admin</NavLink></li>
       </ul>
     </div>
 
