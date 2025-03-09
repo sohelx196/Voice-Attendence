da2739499da038f088cad3dc2dd3f5b2335d9aa3
sohelx196 <sohel.devloper196@gmail.com>
2025-03-09 11:00:05 +0530

    Working..

--- Voice-Attendence-Sdits/src/main.jsx
+++ Voice-Attendence-Sdits/src/main.jsx
@@ -1,6 +1,6 @@
 
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter , RouterProvider} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { router } from '../Router.jsx'
