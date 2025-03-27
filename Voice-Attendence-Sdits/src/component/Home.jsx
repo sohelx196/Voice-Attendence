b8181c8efd37ba16506a1556962483ac7a8bfa9d
sohelx196 <sohel.devloper196@gmail.com>
2025-03-27 14:53:53 +0530

    Working On the main page

--- Voice-Attendence-Sdits/src/component/Home.jsx
+++ Voice-Attendence-Sdits/src/component/Home.jsx
@@ -4,7 +4,7 @@ import Input from "./Input";
 function Home() {
   return (
     <>
-      <div className="relative h-screen w-full flex flex-col md:flex-row justify-center items-center p-10 rounded-t-3xl">
+      <div className="googleFont relative h-screen w-full flex flex-col md:flex-row justify-center items-center p-10 rounded-t-3xl">
   
   
   <div className="flex justify-center items-center">
@@ -20,11 +20,11 @@ function Home() {
       Online Voice Attendance
     </h1>
 
-    <button className="mt-6 px-6 py-3 bg-white text-blue-600 text-lg font-semibold rounded-lg shadow-lg ">
-      Get Started
+    <button className="mt-6 px-6 py-3 bg-white text-black text-lg  rounded-lg 
+     hover:shadow-md hover:shadow-slate-400 hover:transition-all border focus:ring-1 focus:ring-blue-300 font-medium  sm:min-w-fit md:w-1/3 lg:w-1/3">
+      Get Started 
     </button>
   </div>
-
 </div>
 
     </>
